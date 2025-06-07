fb2fd1d3f81acb5f7e5a79c2cd87567833b4e5dc
f3xeek <filipziarko7@gmail.com>
2025-06-07 14:12:56 +0200

    changing profile data

--- server/app/controllers/userController.js
+++ server/app/controllers/userController.js
@@ -7,56 +7,72 @@ const { sign, verify } = jsonwebtoken;
 const { hash, compare } = bcryptjs;
 
 export default {
-    async addNewUser(userData) {
-        const pass = await hash(userData.password, 10)
-        delete userData.password
-        const token = sign({ ...userData, password: pass },
-            process.env.PRIVATE_KEY,
-            { expiresIn: "1m" }
-        )
-        unverifiedUsers.push({ token: token })
-        return token
-    },
-    async verifyUser(token) {
-        const user = unverifiedUsers.filter(user => user.token == token)[0]
-        if (user) {
-            try {
-                const decoded = verify(token, process.env.PRIVATE_KEY)
-                verifiedUsers.push({ ...decoded, id: Date.now(), pfp:null})
-                unverifiedUsers.splice(unverifiedUsers.indexOf(user), 1)
-                return { status: "ok" }
-            } catch (ex) {
-                return { status: "error", message: "token Expired" }
-            }
-        } else return { status: "error", message: "noTokenFound" }
-    },
-    async validateUser(email, password) {
-        const user = verifiedUsers.filter(user => user.email == email)[0]
-        if (user) {
-            const validated = await compare(password, user.password)
-            if (validated) {
-                const token = sign({ email: user.email },
-                    process.env.PRIVATE_KEY,
-                    { expiresIn: "1h" }
-                )
-                return { status: "success", token: token }
-            } else return { status: "error", message: "incorrect credentials" }
-        } else return { status: "error", message: "no user with this email" }
-    },
-    validateUserByToken(token) {
-        try {
-            const decoded = verify(token, process.env.PRIVATE_KEY)
-            if (decoded) {
-                const veri = verifiedUsers.some(user => user.email == decoded.email)
-                if (veri) return decoded.email
-            }
-        } catch (e) { return false }
-    },
-    getProfileData(email){
-        const user = verifiedUsers.filter((user) => user.email == email)[0];
-        if(user){
-            const data = {name:user.name, lastName:user.lastName, pfp:user.pfp}
-            return {status:"success", data:data, images:jsonController.getImagesByAlbum(email)}
-        }else return {status:"error", message:"No user with That email."}
+  async addNewUser(userData) {
+    const pass = await hash(userData.password, 10);
+    delete userData.password;
+    const token = sign(
+      { ...userData, password: pass },
+      process.env.PRIVATE_KEY,
+      { expiresIn: "1m" }
+    );
+    unverifiedUsers.push({ token: token });
+    return token;
+  },
+  async verifyUser(token) {
+    const user = unverifiedUsers.filter((user) => user.token == token)[0];
+    if (user) {
+      try {
+        const decoded = verify(token, process.env.PRIVATE_KEY);
+        verifiedUsers.push({ ...decoded, id: Date.now(), pfp: null });
+        unverifiedUsers.splice(unverifiedUsers.indexOf(user), 1);
+        return { status: "ok" };
+      } catch (ex) {
+        return { status: "error", message: "token Expired" };
+      }
+    } else return { status: "error", message: "noTokenFound" };
+  },
+  async validateUser(email, password) {
+    const user = verifiedUsers.filter((user) => user.email == email)[0];
+    if (user) {
+      const validated = await compare(password, user.password);
+      if (validated) {
+        const token = sign({ email: user.email }, process.env.PRIVATE_KEY, {
+          expiresIn: "1h",
+        });
+        return { status: "success", token: token };
+      } else return { status: "error", message: "incorrect credentials" };
+    } else return { status: "error", message: "no user with this email" };
+  },
+  validateUserByToken(token) {
+    try {
+      const decoded = verify(token, process.env.PRIVATE_KEY);
+      if (decoded) {
+        const veri = verifiedUsers.some((user) => user.email == decoded.email);
+        if (veri) return decoded.email;
+      }
+    } catch (e) {
+      return false;
     }
-}
+  },
+  getProfileData(email) {
+    const user = verifiedUsers.filter((user) => user.email == email)[0];
+    if (user) {
+      const data = { name: user.name, lastName: user.lastName, pfp: user.pfp };
+      return {
+        status: "success",
+        data: data,
+        images: jsonController.getImagesByAlbum(email),
+      };
+    } else return { status: "error", message: "No user with That email." };
+  },
+  changeUserData(data, email){
+    try{
+        const user = verifiedUsers.filter((user) => user.email == email)[0];
+        Object.assign(user,data)
+        return{status:"success"}
+    } catch(e){
+        return {status:error, message:e}
+    }
+    
+  }
+};
--- server/app/routers/profileRouter.js
+++ server/app/routers/profileRouter.js
@@ -1,4 +1,5 @@
-import userController from "../controllers/userController.js" 
+import userController from "../controllers/userController.js"
+import getRequestData from "../utils.js"
 const router = async (req, res, email) => {
     if (req.method == "GET") {
         if (req.url.match(/\/api\/profile\/([\w.-]+@[\w-]+\.[\w.-]{2,4})$/)) {
@@ -7,6 +8,12 @@ const router = async (req, res, email) => {
         } else if(req.url == "/api/profile/self"){
             res.end(JSON.stringify(userController.getProfileData(email),null,5));
         } else res.end(JSON.stringify({status:"error", message:"URL not found"},null,5))
+    }else if (req.method == "PATCH"){
+        if(req.url == "/api/profile"){
+            const body = await JSON.parse(await getRequestData(req));
+            const result = userController.changeUserData(body, email)
+            res.end(JSON.stringify(result, null, 5))
+        }
     }
 }
 export default router
