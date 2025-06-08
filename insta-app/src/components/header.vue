ceabb4bea87adb0394235195a09ec3bc71f8f08d
f3xeek <filipziarko7@gmail.com>
2025-06-08 11:47:12 +0200

    header using primevue

--- insta-app/src/components/header.vue
+++ insta-app/src/components/header.vue
@@ -1,54 +1,47 @@
 <template>
-    <div class="flexnav">
-        <div>
-            <RouterLink to="/" exact>Home</RouterLink>
-            <RouterLink to="/search" exact>Search</RouterLink>
-            <RouterLink to="/about">About</RouterLink>
-            <RouterLink to="/nnn">Not found</RouterLink>
-        </div>
-    </div>
+    <Menubar>
+        <template #start>
+            <div class="flex gap">
+                <RouterLink to="/" class="p-button-text">
+                    Home
+                </RouterLink>
+                <RouterLink to="/about" class="p-button-text">
+                    About
+                </RouterLink>
+                <RouterLink to="/notfound" class="p-button-text">
+                    NotFound
+                </RouterLink>
+            </div>
+        </template>
+
+        <template #end>
+            <div class="flex gap">
+                <RouterLink to="/login" class="p-button-text">
+                    Login
+                </RouterLink>
+                <RouterLink to="/register" class="p-button-text">
+                    Register
+                </RouterLink>
+            </div>
+        </template>
+    </Menubar>
 </template>
 
-<script>
-export default {
-    computed: {
-        user() {
-            return this.$store.getters.GET_CURRENT_USER;
-        },
-        userLoading() {
-            return this.$store.getters.GET_CURRENT_USER_LOADING;
-        },
-    },
-    methods: {
-        logout() {
-            this.$store.dispatch("LOGOUT_USER")
-        }
-    }
-}
+<script setup>
+    import { RouterLink } from 'vue-router'
+
 </script>
 <style scoped>
-div.flexnav {
-    align-items: center;
-    background: #333;
-    margin-bottom: 10px;
-    padding: 10px;
-}
-.flex{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-}
-a, .alice {
-    color: aliceblue;
-    padding: 10px;
-}
+    a{
+        text-decoration: none;
+        color: white;
+        padding: 15px 10px;
+    }
+    .flex{
+        display: flex;
+    }
+    .gap{
+        gap: 30px;
+    }
 
-a:hover {
-    background: #111;
-}
-
-.flexnav {
-    display: flex;
-    justify-content: space-between;
-}
 </style>
--- insta-app/src/main.js
+++ insta-app/src/main.js
@@ -2,5 +2,32 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from "./insta/index"
 import router from "./router"
+import PrimeVue from "primevue/config";
+import InputText from "primevue/inputtext";
+import Password from "primevue/password";
+import Button from "primevue/button";
+import Card from "primevue/card";
+import Menubar from "primevue/menubar";
+import Aura from "@primeuix/themes/aura";
 
-createApp(App).use(store).use(router).mount('#app')
+import "primeicons/primeicons.css";
+
+const app = createApp(App)
+app.use(store)
+app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      prefix: "p",
+      darkModeSelector: "system",
+      cssLayer: false,
+    },
+  },
+});
+app.component("InputText", InputText);
+app.component("Password", Password);
+app.component("Button", Button);
+app.component("Card", Card);
+app.component("Menubar", Menubar);
+app.mount("#app");
